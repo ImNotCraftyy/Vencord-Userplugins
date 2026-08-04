43aec2336d6f44cfc1e3e3a460c2eff8bd43754e
NuzProjects <nuzflamev2@gmail.com>
2026-08-03 20:39:57 -0400

    Update index.tsx

--- staffCrowns/index.tsx
+++ staffCrowns/index.tsx
@@ -140,7 +140,7 @@ const profileBadge: ProfileBadge = {
 
 export default definePlugin({
     name: "Staff Crowns",
-    description: "Adds small Gold, Silver, and Bronze crowns to server owners, admins, and moderators.",
+    description: "Adds a Crown/Tag to Server Owners (or Admins/Management)",
     authors: [{ name: "NuzFlameV2", id: 1248366351194652712n }],
     settings,
     tags: ["Appearance", "Servers"],
