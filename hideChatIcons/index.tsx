0cbb836082e7ffa392c3cbf3ce95b41d74126b44
NuzProjects <nuzflamev2@gmail.com>
2026-08-03 21:12:27 -0400

    Update index.tsx

--- hideChatIcons/index.tsx
+++ hideChatIcons/index.tsx
@@ -1,6 +1,6 @@
 /*
- * hideChatIcons - Vencord Userplugin
- * Created by NuzFlameV2
+ * Vencord, a Discord client mod
+ * Copyright (c) 2026 Vendicated and contributors
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
