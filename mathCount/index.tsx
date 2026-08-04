3f1e7dcb05da0bf4af2b4a7cd8aec188d618663c
NuzProjects <nuzflamev2@gmail.com>
2026-08-03 21:12:35 -0400

    Update index.tsx

--- mathCount/index.tsx
+++ mathCount/index.tsx
@@ -1,6 +1,6 @@
 /*
- * mathCounter - Vencord Userplugin
- * Created by NuzFlameV2
+ * Vencord, a Discord client mod
+ * Copyright (c) 2026 Vendicated and contributors
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
